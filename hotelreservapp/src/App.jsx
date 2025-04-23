84af5da330f4fa0320b81eb819755d6c046bfdf1
winsthonapazamamani <winsthonapazamamani@gmail.com>
2025-04-23 13:03:07 -0500

    WIP: changes in App.jsx

--- hotelreservapp/src/App.jsx
+++ hotelreservapp/src/App.jsx
@@ -1,8 +1,9 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './pages/Home';
-
+import Home from './pages/Home'; // ← asegúrate de que exista
+import About from './pages/About';
 
 function App() {
   return (
@@ -10,10 +11,9 @@ function App() {
       <div className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow">
-          {/* Navigation menu paths */}
           <Routes>
-            <Route path="/" element={<Home />} />            
-            
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
           </Routes>
         </main>
         <Footer />
