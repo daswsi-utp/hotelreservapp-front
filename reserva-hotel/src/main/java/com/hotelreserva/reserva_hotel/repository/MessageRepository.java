02d3aa5d51f6282343f4c9b105c13aba137e84dc
R0dn3 <u19305755@utp.edu.pe>
2025-05-19 16:55:23 -0500

    Add controller folder

--- reserva-hotel/src/main/java/com/hotelreserva/reserva_hotel/repository/MessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hotelreserva.reserva_hotel.repository;
-
-import com.hotelreserva.reserva_hotel.model.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<Message, Long> {
-}
