b96997dc0737644ef86287b7e90b9083ea98e281
R0dn3 <u19305755@utp.edu.pe>
2025-05-20 22:31:33 -0500

    add config folder - RedisConfig file

---
+++ reserva-hotel_Backend/src/main/java/com/hotelreserva/reserva_hotel/config/RedisConfig.java
@@ -0,0 +1,18 @@
+package com.hotelreserva.reserva_hotel.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10)) // Cache expira después de 10 minutos
+                .disableCachingNullValues();
+    }
+}
